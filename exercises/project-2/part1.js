f2297f0c7b31e057b1c3a34190e245892397e90c
houseofaragon <saintkaren@gmail.com>
2024-09-09 15:39:07 -0400

    more poc

---
+++ exercises/project-2/part1.js
@@ -0,0 +1,38 @@
+const path = './model/'
+const startButton = document.getElementById('start')
+
+startButton.onclick = () => init()
+
+let model, webcam
+
+const init = async () => {
+    try {
+        const modelPath = path + 'model.json'
+        const metadataPath = path + 'metadata.json'
+
+        model = await tmImage.load(modelPath, metadataPath)
+
+        webcam = new tmImage.Webcam(200, 200, false) // false to flip camera
+        await webcam.setup() // request access to camera
+        await webcam.play()
+
+        window.requestAnimationFrame(loop)
+
+        let maxPredictions = model.getTotalClasses()
+
+        document.getElementById('webcam-container').appendChild(webcam.canvas)
+        console.log(`Model loaded successfully. Number of predictions: ${maxPredictions}`)
+    } catch (error) {
+        console.error('Error loading model or setting up webcam:', error)
+    }
+}
+
+const loop = async () => {
+    webcam.update()
+    await predict()
+}
+
+const predict = async () => {
+    const predictions = await model.predict(webcam.canvas)
+    console.log(predictions)
+}
