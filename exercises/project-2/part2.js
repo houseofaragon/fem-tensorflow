422de960d981b5721604bc8fe8c6220f539a74b6
houseofaragon <saintkaren@gmail.com>
2024-10-02 14:16:21 -0400

    testing

--- exercises/project-2/part2.js
+++ exercises/project-2/part2.js
@@ -16,6 +16,20 @@ const denseUnits = 100
 let isTraining = false
 let isPredicting = false
 
-const loadModel = async = () => {
-    
+const loadModel = async () => {
+    const mobilenet = await tf.loadLayersModel("https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json")
+
+    const layer = mobilenet.getLayer('conv_pw_13_relu')
+
+    return tf.model({ inputs: mobilenet.inputs, outputs: layer.output})
 }
+
+const init = async () => {
+    webcam = await tfd.webcam(document.getElementById('webcam'))
+
+    initialModel = await loadModel(); 
+    statusElement.style.display = 'none'
+    document.getElementById('controller').style.display = 'block'
+}
+
+init();
